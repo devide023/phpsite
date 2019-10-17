8987bd14b83fafe248638467317109f9f76e6e5d
devide <devide@126.com>
2019-10-17 11:30:39 +0800

    1

---
+++ app/Models/Organize.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organize extends Model
+{
+    //
+    protected $table='organize';
+    public $timestamps =false;
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -28,13 +28,5 @@ class User extends Model
 {
     //
     protected $table='user';
-    protected $primaryKey='id';
     public $timestamps =false;
-    protected $guarded=[];
-
-    public function files()
-    {
-        return $this->hasMany('App\Models\Userfile','userid','id');
-    }
-
 }
